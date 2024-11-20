a9aab8b1b599781a3e40f52a75784ee245c8327b
MaheshWaghmare13 <mahesh.beed2018@gmail.com>
2024-11-20 17:21:10 +0530

    update

--- lms/lmsapp/urls.py
+++ lms/lmsapp/urls.py
@@ -3,5 +3,5 @@ from django.urls import path , include
 from lmsapp import views
 
 urlpatterns = [
-    
+    path('',views.index)
 ]
--- lms/lmsapp/views.py
+++ lms/lmsapp/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 
 # Create your views here.
+
+
+
+def index(request):
+    return render(request , 'index.html')
