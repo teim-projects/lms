3d80206a273047ed2f8816d1b219d5bb3986041d
teim-projects <108449533+teim-projects@users.noreply.github.com>
2025-01-18 17:37:42 +0530

    Update in subadmin

--- lms/lmsapp/urls.py
+++ lms/lmsapp/urls.py
@@ -30,9 +30,6 @@ urlpatterns = [
     path('manage_subadmins/', views.manage_subadmins, name='manage_subadmins'),
     path('subadmin_dashboard/', views.subadmin_dashboard, name='subadmin_dashboard'),
     path('subadmin-login/', views.subadmin_login_view, name='subadmin_login'),
-
-
-
 ]
 
 
