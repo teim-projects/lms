c9e8362841113a3a269f9bdc74a0cacecc88102b
MaheshWaghmare13 <sonuwaghmare7057@gmail.com>
2025-01-29 11:07:18 +0530

    login restriction for more device

--- lms/lmsapp/apps.py
+++ lms/lmsapp/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class LmsappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'lmsapp'
+
+    def ready(self):
+        import lmsapp.signals  # Import signals to connect
